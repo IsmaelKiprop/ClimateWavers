9970326e727f29ef158704c9de6a946591e22cd0
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-09-30 13:34:41 -0400

    node api now able to retrieve data from third party api and store it in mongo db database

--- nodeApi/src/controllers/earthQuakeController.js
+++ nodeApi/src/controllers/earthQuakeController.js
@@ -1,20 +1,36 @@
 const axios = require('axios');
-const EarthquakeData = require('../models/earthQuakeData');
+const EarthquakeData = require('../models/earthQuakeData'); // Assuming your model file is named 'earthquakeData'
 
 async function fetchAndStoreEarthquakeData(req, res) {
   try {
-    // Make an API request to fetch earthquake data from USGS API
-    const response = await axios.get('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2000-01-01&endtime=2023-09-30');
-    const earthquakeData = response.data.features;
+    // Make an API request to fetch earthquake data from the new API
+    const response = await axios.get('https://api.orhanaydogdu.com.tr/deprem/kandilli/live');
+    const earthquakeData = response.data.result;
 
     // Store the earthquake data in MongoDB
-    for (const feature of earthquakeData) {
-      const { mag, place, time } = feature.properties;
+    for (const earthquakeItem of earthquakeData) {
+      const {
+        _id,
+        earthquake_id,
+        provider,
+        title,
+        date,
+        mag,
+        depth,
+        geojson,
+        location_properties,
+      } = earthquakeItem;
 
       const earthquake = new EarthquakeData({
-        magnitude: mag,
-        location: place,
-        timestamp: new Date(time),
+        _id,
+        earthquake_id,
+        provider,
+        title,
+        date: new Date(date),
+        mag,
+        depth,
+        geojson,
+        location_properties,
       });
 
       await earthquake.save();
