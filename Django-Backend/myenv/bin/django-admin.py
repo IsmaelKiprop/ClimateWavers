47e134b60c1ef9e4405f4039a929eb36d360d15b
IsmaelKiprop <kipropismael27@gmail.com>
2023-10-04 05:33:54 +0300

    updated

---
+++ Django-Backend/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/ISMAEL/OneDrive/Desktop/ClimateWavers/Django-Backend/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
